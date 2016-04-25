2960d55d43f3aa8a556581250a3ea0b37f28b16c
marina kotlyarova <mar.kotlyarova@gmail.com>
2016-04-25 17:40:31 +0300

    added home_js_7

---
+++ Home_js_7/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('input').on('mouseenter', function() {
+    $(this).siblings('span').show();
+  });
+  $('input').on('mouseleave', function() {
+    $(this).siblings('span').hide();
+  });
+  $('.help').on('click', function(event) {
+    event.preventDefault();
+    $('span').show();
+  })
+});
+
